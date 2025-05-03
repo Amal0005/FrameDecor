0b850fe2dc277be921cec88d737af11c35b426c8
Amal0005 <amalachu441345@gmail.com>
2025-05-03 20:38:48 +0530

    signup page created

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,4 +1,4 @@
-// const router=require("./router/")
+const User=require("../../models/userSchema")
 
 const pageNotFound=async(req,res)=>{
     try {
@@ -15,8 +15,31 @@ const loadHome=async(req,res)=>{
         res.status(500).send("server error")
     }
 }
+const loadSignup=async(req,res)=>{
+    try {
+        res.render("signup")
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("server error")
+    }
+}
+const signup=async(req,res)=>{
+    const{name,mobile,password,email}=req.body
+    try {
+        const newUser=new User({name,mobile,password,email})
+        await newUser.save()
+        console.log(newUser)
+       return  res.redirect("/signup")
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("server error")
+    }
+
+}
 
 module.exports={
     loadHome,
-    pageNotFound
+    pageNotFound,
+    loadSignup,
+    signup
 }
