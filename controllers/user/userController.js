9bbadab2ce53594e7e0aa7bafb007e187dae812c
Amal0005 <amalachu441345@gmail.com>
2025-05-05 21:50:27 +0530

    generate otp

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,4 +1,6 @@
 const User=require("../../models/userSchema")
+const nodemailer=require("nodemailer")
+const env=require("dotenv")
 
 const pageNotFound=async(req,res)=>{
     try {
@@ -23,18 +25,74 @@ const loadSignup=async(req,res)=>{
         res.status(500).send("server error")
     }
 }
-const signup=async(req,res)=>{
-    const{name,mobile,password,email}=req.body
-    try {
-        const newUser=new User({name,mobile,password,email})
-        await newUser.save()
-        console.log(newUser)
-       return  res.redirect("/signup")
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("server error")
-    }
+// const signup=async(req,res)=>{
+//     const{name,mobile,password,email}=req.body
+//     try {
+//         const newUser=new User({name,mobile,password,email})
+//         await newUser.save()
+//         console.log(newUser)
+//        return  res.redirect("/signup")
+//     } catch (error) {
+//         console.error(error)
+//         res.status(500).send("server error")
+//     }
 
+// }
+function generateOtp(){
+    return Math.floor(100000+Math.random()*900000).toString()
+}
+async function sendVerificationEmail(email,otp){
+    try {
+        const transporter=nodemailer.createTransport({
+            service:"gmail",
+            port:587,
+            secure:false,
+            requireTLS:true,
+            auth:{
+                user:process.env.NODEMAILER_EMAIL,
+                pass:process.env.NODEMAILER_PASSWORD
+            }
+        })
+
+         const info=await transporter.sendMail({
+            from:process.env.NODEMAILER_EMAIL,
+                to: email,
+                subject:"verify your Account",
+                text:`Your otp is ${otp}`,
+                html:`<b>Your otp ${otp}</b>`,
+         })
+         return info.accepted.length>0
+
+    } catch (error) {
+        console.error("error sending email",error);
+        return false;
+    }
+}
+
+const signup=async(req,res)=>{
+    const{email,password,cPassword}=req.body
+    try {
+        if(password!==cPassword){
+            res.render("signup",{message:"Passwords are not matching"})
+        }
+        const findUser=await User.findOne({email:req.body.email})
+        if(findUser){
+            res.render("signup",{message:"User already exist"})
+        }
+        const otp=generateOtp()
+        const emailSent=await sendVerificationEmail(email,otp)
+        if(!emailSent){
+            return res.json("email error")
+        }
+        req.session.userOtp=otp;
+        req.session.userData={email,password}
+        // res.render("verify-otp");
+        console.log('otp send',otp)
+
+    } catch (error) {
+        console.error("signup error",error);
+        res.redirect("/pageNotFound")
+    }
 }
 
 module.exports={
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const session=require("express-session")
 const app=express()
 const Dotenv=require('dotenv').config()
 const db=require('./Config/db')
@@ -14,6 +15,22 @@ app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/ad
 
 app.use(express.static(path.join(__dirname,"public")))
 
+
+app.use(session({
+    secret:process.env.SECRET_KEY,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        secure:false,
+        httpOnly:true,
+        maxAge:72*60*60*1000
+    }
+    
+}))
+app.use((req,res,next)=>{
+    res.set("cache-controll","no-store")
+    next()
+})
 app.use("/",userRouter)
 
 const PORT = process.env.PORT || 1212;
