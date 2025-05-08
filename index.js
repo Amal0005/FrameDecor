2d440c81b2e872944c2db6c695b9018c158cb72a
Amal0005 <amalachu441345@gmail.com>
2025-05-08 20:46:37 +0530

    changed the styles

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req,res,next)=>{
     res.set("cache-controll","no-store")
     next()
 })
-app.use("/",userRouter)
+app.use("/",userRouter) 
 
 const PORT = process.env.PORT || 1212;
 app.listen(PORT, () => {
